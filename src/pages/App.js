43c10fdcc14ac734d7c698ba4bdc6255b42a891c
Saull Brandão <saull@outlook.com>
2021-10-23 09:24:42 -0300

    feat: display a quote, a button  and a image on the homepage

--- src/pages/App.js
+++ src/pages/App.js
@@ -1,10 +1,27 @@
+import styled from 'styled-components'
+import socratesImg from '../assets/images/socrates.png'
+import { Quote } from '../components/Quote'
 import './App.css'
+
+const Content = styled.div`
+  height: 100vh;
+  padding: 0 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const SocratesImg = styled.img`
+  max-width: 50vw;
+  align-self: flex-end;
+`
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Content className="App">
+      <Quote quote="ok" speaker="random" />
+      <SocratesImg src={socratesImg} alt="Socrates" />
+    </Content>
   )
 }
 
--- src/pages/App.test.js
+++ src/pages/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders the app', () => {
+test('renders the app with a quote, a image and a button', () => {
   render(<App />)
-  const text = screen.getByText(/Hello World/i)
-  expect(text).toBeInTheDocument()
+
+  const buttonEl = screen.getByRole('button')
+  const textEl = screen.getByText(/random/i)
+  const imageEl = screen.getByRole('img')
+
+  expect(buttonEl).toBeInTheDocument()
+  expect(imageEl).toBeInTheDocument()
+  expect(textEl).toBeInTheDocument()
 })
